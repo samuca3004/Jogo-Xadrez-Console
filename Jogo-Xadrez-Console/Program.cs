47670c0da2bb12defed384b3c519af756da6306f
Samuel Ribeiro <samuca3004@hotmail.com>
2022-03-01 16:54:26 -0300

    Add project files.

---
+++ Jogo-Xadrez-Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace Jogo_Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.WriteLine();
+
+        }
+    }
+}
