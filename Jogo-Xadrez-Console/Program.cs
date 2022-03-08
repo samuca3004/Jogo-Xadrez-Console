b391772ba43f872f49c02615b8dcd44d324e2498
Samuel Ribeiro <samuca3004@hotmail.com>
2022-03-08 10:35:23 -0300

    Cor peças

--- Jogo-Xadrez-Console/Program.cs
+++ Jogo-Xadrez-Console/Program.cs
@@ -19,7 +19,7 @@ namespace Jogo_Xadrez_Console
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
 
                 Tela.imprimirTabuleiro(tab);
 
